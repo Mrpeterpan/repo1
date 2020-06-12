7a20bfb22e4b25f9df636b1bcf4756b00a958a74
mrpeterpan <1501406553@qq.com>
2020-06-12 12:32:48 +0800

    把HelloWorld工程添加到版本库

---
+++ 1.HelloWorld/src/com/first/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.first.demo;
+//我的第一个JAVA程序
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("告诉我为什么");
+        System.out.println(1234);
+        System.out.println('a');
+        //空常量不能直接打印
+
+    }
+}
